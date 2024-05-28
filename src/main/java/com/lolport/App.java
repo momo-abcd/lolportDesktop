549e9b9cbc348d1d9d30722af40a6bd16735c36b
양윤모 <yym4481@naver.com>
2024-05-28 17:30:12 +0900

    javafx 테스트 겸 공부

--- src/main/java/com/lolport/App.java
+++ src/main/java/com/lolport/App.java
@@ -17,13 +17,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+//        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"));
         stage.setScene(scene);
+        stage.setTitle("연습!");
+        stage.sizeToScene();
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+        Stage stage = (Stage) scene.getWindow();
+//        stage.sizeToScene();
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
