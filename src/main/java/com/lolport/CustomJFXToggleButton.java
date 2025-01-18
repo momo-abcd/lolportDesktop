3d24f65d296ebe865d52cf7846c1a2924e4d934a
양윤모 <yym4481@naver.com>
2025-01-18 16:11:27 +0900

    feat: 동그라미 녹화중 애니메이션 구현 (끊기는 현상 해결해야함)

--- src/main/java/com/lolport/CustomJFXToggleButton.java
+++ src/main/java/com/lolport/CustomJFXToggleButton.java
@@ -3,21 +3,22 @@ package com.lolport;
 import com.jfoenix.controls.JFXToggleButton;
 import com.jfoenix.skins.JFXToggleButtonSkin;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.scene.control.Skin;
+import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Effect;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
 
 import java.lang.reflect.Field;
 
+
 public class CustomJFXToggleButton extends JFXToggleButton {
 
-    Timeline timeline = new Timeline();
+    Timeline t1 = new Timeline();
+    SequentialTransition sq = new SequentialTransition();
+
 
     public CustomJFXToggleButton() {
         super();
@@ -40,27 +41,60 @@ public class CustomJFXToggleButton extends JFXToggleButton {
 //        } catch (NoSuchFieldException | IllegalAccessException e) {
 //            throw new RuntimeException(e);
 //        }
-        
+
+//        DropShadow dropShadow = new DropShadow(BlurType.THREE_PASS_BOX, Color.rgb(173, 0,0,.3), 40, 0.9, 0, 0);
+
+
         // 버튼이 열리는 경우 애니메이션 실행
         // true가 열린 상태이겠지..?
-        if(super.selectedProperty().get()) {
-//            Timeline timeline = new Timeline();
-            timeline.setCycleCount(Timeline.INDEFINITE);
-            timeline.setAutoReverse(true);
-            String css = "-fx-effect:dropshadow( two-pass-box ,  rgba(173,0,0,.3), 40, 0.9, 0, 0)";
-            DropShadow dropShadow = new DropShadow();
+//        Timeline t1 = new Timeline();
+        Timeline t2 = new Timeline();
+        if (super.selectedProperty().get()) {
 
+            Circle thumb = getCircleNode();
 
-            KeyValue kv = new KeyValue(CustomJFXToggleButtonSkin.thumb.styleProperty(), css);
-            KeyFrame kf = new KeyFrame(Duration.millis(1500), kv);
-            timeline.getKeyFrames().add(kf);
-            timeline.play();
-            
+            DropShadow dropShadow = new DropShadow(BlurType.GAUSSIAN, Color.rgb(173, 0, 0, .4), 25, 0, 0, 0);
+            thumb.setEffect(dropShadow);
+
+            Timeline firstTimeline = new Timeline();
+            Timeline secondTimeline = new Timeline();
+            Timeline thirdTimeline = new Timeline();
+
+            KeyValue spreadKeyValue = new KeyValue(dropShadow.spreadProperty(), .9);
+            KeyValue colorKeyValue = new KeyValue(dropShadow.colorProperty(), Color.rgb(173, 0, 0, 0));
+
+            firstTimeline.setCycleCount(1);
+            secondTimeline.setCycleCount(1);
+            thirdTimeline.setCycleCount(1);
+
+            firstTimeline.getKeyFrames().add(new KeyFrame(Duration.millis(975), spreadKeyValue));
+            secondTimeline.getKeyFrames().add(new KeyFrame(Duration.millis(500), colorKeyValue));
+
+            sq.getChildren().addAll(firstTimeline, secondTimeline);
+            sq.setCycleCount(Animation.INDEFINITE);
+            sq.setDelay(Duration.millis(400));
+            sq.play();
+
+//
+////            KeyValue kv3 = new KeyValue(dropShadow1.colorProperty(), Color.rgb(13, 0,0,0), Interpolator.LINEAR);
+//            KeyFrame kf2 = new KeyFrame(Duration.millis(375), kv3);
+//            t2.getKeyFrames().add(kf2);
+//            KeyValue kv4 = new KeyValue(dropShadow1.colorProperty(), Color.BLUEVIOLET, Interpolator.LINEAR);
+//            KeyFrame kf3 = new KeyFrame(Duration.millis(3000), kv4);
+//            Timeline t3 = new Timeline(kf3);
+
+//            SequentialTransition sq = new SequentialTransition(t1,t2);
+//            sq.play();
+
         }
         // 버튼이 닫히는 경우 애니메이션 종료
         else {
-            timeline.stop();
+            sq.stop();
         }
+    }
+
+    private Circle getCircleNode() {
+        return CustomJFXToggleButtonSkin.thumb;
     }
 
     @Override
