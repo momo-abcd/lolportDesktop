a4dcc69a7bf99d3e2b180bf19e01e311da76c2aa
양윤모 <yym4481@naver.com>
2025-01-18 10:46:18 +0900

    tmp: css가 아닌 DropShadow 클래스로 애니메이션 작성하는중

--- src/main/java/com/lolport/CustomJFXToggleButton.java
+++ src/main/java/com/lolport/CustomJFXToggleButton.java
@@ -3,7 +3,12 @@ package com.lolport;
 import com.jfoenix.controls.JFXToggleButton;
 import com.jfoenix.skins.JFXToggleButtonSkin;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.scene.control.Skin;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Effect;
 import javafx.scene.shape.Circle;
 import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
@@ -12,9 +17,50 @@ import java.lang.reflect.Field;
 
 public class CustomJFXToggleButton extends JFXToggleButton {
 
+    Timeline timeline = new Timeline();
+
     public CustomJFXToggleButton() {
         super();
         this.getStylesheets().add(getClass().getResource("/css/navbar.css").toExternalForm());
+    }
+
+    // 동그라미에 애니메이션 효과 주기 위한 Event 처리 메서드
+    @Override
+    public void fire() {
+        super.fire();
+
+        // 동그라미 애니메이션 관련 코드
+        // 리플랙션으로 동그라미 Node 가지고 옴
+//        Circle thumb = CustomJFXToggleButtonSkin.thumb;
+//        Field thumbField = null;
+//        try {
+//            thumbField = JFXToggleButtonSkin.class.getDeclaredField("circle");
+//            thumbField.setAccessible(true);
+//            thumb = (Circle) thumbField.get(this);
+//        } catch (NoSuchFieldException | IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+        
+        // 버튼이 열리는 경우 애니메이션 실행
+        // true가 열린 상태이겠지..?
+        if(super.selectedProperty().get()) {
+//            Timeline timeline = new Timeline();
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.setAutoReverse(true);
+            String css = "-fx-effect:dropshadow( two-pass-box ,  rgba(173,0,0,.3), 40, 0.9, 0, 0)";
+            DropShadow dropShadow = new DropShadow();
+
+
+            KeyValue kv = new KeyValue(CustomJFXToggleButtonSkin.thumb.styleProperty(), css);
+            KeyFrame kf = new KeyFrame(Duration.millis(1500), kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.play();
+            
+        }
+        // 버튼이 닫히는 경우 애니메이션 종료
+        else {
+            timeline.stop();
+        }
     }
 
     @Override
@@ -24,7 +70,7 @@ public class CustomJFXToggleButton extends JFXToggleButton {
 
     public static class CustomJFXToggleButtonSkin extends JFXToggleButtonSkin {
 
-        private Circle thumb;
+        static Circle thumb;
 
         public CustomJFXToggleButtonSkin(JFXToggleButton control) {
             super(control);
