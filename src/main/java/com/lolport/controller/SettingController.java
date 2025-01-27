8b9ac1d8748aae7dbf88769012b01891e5f716b5
양윤모 <yym4481@naver.com>
2025-01-27 20:05:00 +0900

    tmp: fxml과 controller 연결 작업중
    각 설정 버튼을 누르면 stage는 안보이고 오버레이 박스만 보여서 크기를 조절할 수 있게 구현해야함

---
+++ src/main/java/com/lolport/controller/SettingController.java
@@ -0,0 +1,58 @@
+package com.lolport.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.lolport.capture.overlay.ChatOverlayImpl;
+import com.lolport.capture.overlay.OverLay;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingController implements Initializable {
+    @FXML
+    private JFXButton fullSettingBtn;
+    @FXML
+    private JFXButton chatSettingBtn;
+    @FXML
+    private JFXButton tabSettingBtn;
+//    private final Stage stage; // 블루레이 사각형 설정시 프로그램 투명화를 위해 필요
+//    private final Scene settingScene;
+//    private final VBox contentSide; // 프로그램의 오른쪽 화면에대한 노드 (dock 사이드, 컨탠츠 사이드 두 개 존재)
+
+    public SettingController(){};
+
+//    public SettingController(Stage stage, Scene settingScene) {
+//        this.stage = stage;
+//        this.settingScene = settingScene;
+//        this.contentSide = new VBox(); // 이 요소에 설정탭에 관련된 UI들을 배치하는거임
+//
+//        Button btn1 = new Button("채팅창오버레이 설정");
+//        Button btn2 = new Button("탭창오버레이 설정");
+//        Button btn3 = new Button("전체화면오버레이 설정");
+//        this.contentSide.getChildren().addAll(btn1, btn2, btn3);
+//
+//        btn1.setOnAction(actionEvent -> {
+//            OverLay chatOverLay = new ChatOverlayImpl(stage, settingScene, contentSide);
+//            System.out.println("눌림");
+//        });
+//        btn2.setOnAction(actionEvent -> {
+//
+//        });
+//        btn3.setOnAction(actionEvent -> {
+//
+//        });
+//        // SCENE 전환
+////        settingScene.setRoot(this.contentSide );
+//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
